600a3255befeff747c4f95405a588f3c613062eb
kerexaaa <126904760+kerexaaa@users.noreply.github.com>
2023-10-18 15:59:48 +0300

    via

---
+++ script.js
@@ -0,0 +1,18 @@
+// switch(optionFrom) {
+//     case 'BINARY':
+//         for(let i = 0; i < inputFrom.value.length; i++) {
+//             if(inputFrom.value[i] !== 0 || inputFrom.value[i] !== 1){
+//                 window.alert('Input may contain only 0 or 1 numbers!')
+//             }
+//         }
+//     break;
+//     case 'ROMAN':
+//         const letters = /^[A-Za-z]+$/;
+//         if(inputFrom.value.match(letters)){
+//                 return true;
+//             }
+//         else {
+//             window.alert('Input may contain only letters!')
+//             return false;
+//             }
+// }
